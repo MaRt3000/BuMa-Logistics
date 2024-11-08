72c2b1f01ccbf8a3e675f7124b180fd6a911ab25
uzeez9 <uzeez90@gmail.com>
2024-11-08 10:31:30 +0100

    Uyi's Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Reason from './components/Reason'
 import Header from '../src/components/Header.jsx'
 import HeroSection from '../src/components/HeroSection.jsx'
+import GlobalWithEase from './components/GlobalWithEase.jsx'
 import Footer from '../src/components/Footer.jsx'
-
 function App() {
   
   return (
     <>
     <Header />
     <HeroSection />
+    <GlobalWithEase />
      <Reason />
      <Footer />
     </>
